ee5a91297d26116182e88ae7386d5126b0e7f82b
Ugur Ozturk <ugurozturk918@gmail.com>
2024-07-30 22:25:51 +0200

    fix: leader-election options and some other fixes
    Signed-off-by: Ugur Ozturk <ugurozturk918@gmail.com>

--- main.go
+++ main.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -52,22 +36,34 @@ func main() {
 		kubeconfig           string
 		healthProbeAddr      string
 		enableLeaderElection bool
+		leaseDuration        time.Duration
+		renewDeadline        time.Duration
+		retryPeriod          time.Duration
+		resourceLock         string
+		resourceName         string
 	)
 
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&apiServerURL, "apiserver", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&healthProbeAddr, "health-probe-address", ":8081", "Specifies the TCP address for the health server to listen on.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", true, "Enable leader election for the controller manager. Ensures there is only one active controller manager.")
+	flag.DurationVar(&leaseDuration, "leader-elect-lease-duration", 15*time.Second, "Duration that non-leader candidates will wait to force acquire leadership (duration string).")
+	flag.DurationVar(&renewDeadline, "leader-elect-renew-deadline", 10*time.Second, "Interval between attempts by the acting master to renew a leadership slot before it stops leading (duration string).")
+	flag.DurationVar(&retryPeriod, "leader-elect-retry-period", 2*time.Second, "Duration the clients should wait between attempting acquisition and renewal of a leadership (duration string).")
+	flag.StringVar(&resourceLock, "leader-elect-resource-lock", "leases", "The type of resource object that is used for locking. Supported options are 'leases', 'endpoints', 'configmaps'.")
+	flag.StringVar(&resourceName, "leader-elect-resource-name", "node-ipam-controller", "The name of the resource object that is used for locking.")
 
 	c := logsapi.NewLoggingConfiguration()
 	logsapi.AddGoFlags(c, flag.CommandLine)
 	flag.Parse()
+
 	logs.InitLogs()
 	if err := logsapi.ValidateAndApply(c, nil); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	ctx := signals.SetupSignalHandler()
+	ctx, cancel := context.WithCancel(signals.SetupSignalHandler())
+	defer cancel()
 	logger := klog.FromContext(ctx)
 
 	server := startHealthProbeServer(healthProbeAddr, logger)
@@ -85,15 +81,21 @@ func main() {
 
 	if enableLeaderElection {
 		logger.Info("Leader election is enabled.")
-		leaderelection.StartLeaderElection(ctx, kubeClient, func(ctx context.Context) {
-			runControllers(ctx, kubeClient, cfg, logger)
+		go leaderelection.StartLeaderElection(ctx, kubeClient, cfg, logger, cancel, runControllers, leaderelection.Config{
+			LeaseDuration: leaseDuration,
+			RenewDeadline: renewDeadline,
+			RetryPeriod:   retryPeriod,
+			ResourceLock:  resourceLock,
+			ResourceName:  resourceName,
 		})
 	} else {
 		logger.Info("Leader election is disabled.")
-		runControllers(ctx, kubeClient, cfg, logger)
+		go runControllers(ctx, kubeClient, cfg, logger)
 	}
 
-	if err := server.Shutdown(ctx); err != nil {
+	<-ctx.Done()
+	logger.Info("Shutting down server")
+	if err := server.Shutdown(context.Background()); err != nil {
 		logger.Error(err, "failed to shut down health server")
 	}
 }
--- pkg/leaderelection/leaderelection.go
+++ pkg/leaderelection/leaderelection.go
@@ -6,13 +6,23 @@ import (
 	"time"
 
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/leaderelection"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/klog/v2"
 )
 
+// Config holds the configuration parameters for leader election
+type Config struct {
+	LeaseDuration time.Duration
+	RenewDeadline time.Duration
+	RetryPeriod   time.Duration
+	ResourceLock  string
+	ResourceName  string
+}
+
 // StartLeaderElection starts the leader election process
-func StartLeaderElection(ctx context.Context, kubeClient kubernetes.Interface, runFunc func(ctx context.Context)) {
+func StartLeaderElection(ctx context.Context, kubeClient kubernetes.Interface, cfg *rest.Config, logger klog.Logger, cancel context.CancelFunc, runFunc func(ctx context.Context, kubeClient kubernetes.Interface, cfg *rest.Config, logger klog.Logger), config Config) {
 	id := os.Getenv("POD_NAME")
 	if id == "" {
 		klog.Fatalf("POD_NAME environment variable not set")
@@ -24,9 +34,9 @@ func StartLeaderElection(ctx context.Context, kubeClient kubernetes.Interface, r
 	}
 
 	rl, err := resourcelock.New(
-		resourcelock.LeasesResourceLock,
+		config.ResourceLock,
 		namespace,
-		"node-ipam-controller",
+		config.ResourceName,
 		kubeClient.CoreV1(),
 		kubeClient.CoordinationV1(),
 		resourcelock.ResourceLockConfig{
@@ -38,22 +48,24 @@ func StartLeaderElection(ctx context.Context, kubeClient kubernetes.Interface, r
 	}
 
 	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
-		Lock:          rl,
-		LeaseDuration: 15 * time.Second,
-		RenewDeadline: 10 * time.Second,
-		RetryPeriod:   2 * time.Second,
+		Lock:            rl,
+		LeaseDuration:   config.LeaseDuration,
+		RenewDeadline:   config.RenewDeadline,
+		RetryPeriod:     config.RetryPeriod,
+		ReleaseOnCancel: true,
 		Callbacks: leaderelection.LeaderCallbacks{
 			OnStartedLeading: func(ctx context.Context) {
-				klog.Info("Started leading")
-				runFunc(ctx)
+				klog.Infof("Started leading as %s", id)
+				runFunc(ctx, kubeClient, cfg, logger)
 			},
 			OnStoppedLeading: func() {
-				klog.Info("Stopped leading")
-				klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+				klog.Infof("%s stopped leading", id)
+				// Instead of exiting, cancel the context to trigger the shutdown sequence
+				cancel()
 			},
 			OnNewLeader: func(identity string) {
 				if identity == id {
-					klog.Info("I am the new leader")
+					klog.Infof("I am the new leader: %s", id)
 				} else {
 					klog.Infof("New leader elected: %s", identity)
 				}
